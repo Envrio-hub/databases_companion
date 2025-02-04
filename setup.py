5af3314932bf02102bccb4308a6cb11b8be23842
itsakmak <xylopodaros@yahoo.gr>
2025-02-04 11:58:03 +0200

    renaming to database_companion

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='db_companion',
+    name='database_companion',
     version='0.1.0',
     description='Auxiliary modules for database libraries',
     author='Ioannis Tsakmakis, Nikolaos Kokkos',
