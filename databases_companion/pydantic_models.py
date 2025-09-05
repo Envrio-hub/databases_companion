3699339e5baaba6473dbf87b104cf25e3d9900af
itsakmak <xylopodaros@yahoo.gr>
2025-09-06 01:17:14 +0300

    WaveDataArgs and CurrentTempChlorophyllDataArgs classes

--- databases_companion/pydantic_models.py
+++ databases_companion/pydantic_models.py
@@ -1,13 +1,13 @@
-__version__='0.1.1'
+__version__='0.1.2'
 __author__=['Ioannis Tsakmakis']
 __date_created__='2025-08-20'
-__last_updated__='2025-09-05'
+__last_updated__='2025-09-06'
 
 from pydantic import BaseModel, condecimal, validator
 from decimal import Decimal
 from typing import Annotated
 from datetime import datetime
-from enum_variables import BeachTypes
+from databases_companion.enum_variables import BeachTypes
 
 class BeachAmenities(BaseModel):
     sunbeds: bool = False
@@ -19,9 +19,9 @@ class BeachAmenities(BaseModel):
     watersports: bool = False
     lifeguard: bool = False
 
-class CMEMSPointData(BaseModel):
-    longitude: Annotated[Decimal, condecimal(max_digits=10, decimal_places=6)]
-    latitude: Annotated[Decimal, condecimal(max_digits=10, decimal_places=6)]
+class WaveDataArgs(BaseModel):
+    beach_longitude: Annotated[Decimal, condecimal(max_digits=10, decimal_places=6)]
+    beach_latitude: Annotated[Decimal, condecimal(max_digits=10, decimal_places=6)]
     start_date: datetime
     end_date: datetime
     process_waves: bool
@@ -36,5 +36,19 @@ class CMEMSPointData(BaseModel):
                 return datetime.strptime(value, "%Y-%m-%d")
             except ValueError:
                 raise ValueError("Dates must be in format YYYY-MM-DD")
-        return value
+        return value 
     
+class CurrentTempChlorophyllDataArgs(BaseModel):
+    longitude: Annotated[Decimal, condecimal(max_digits=10, decimal_places=6)]
+    latitude: Annotated[Decimal, condecimal(max_digits=10, decimal_places=6)]
+    start_date: datetime
+    end_date: datetime
+
+    @validator("start_date", "end_date", pre=True)
+    def parse_dates(cls, value):
+        if isinstance(value, str):
+            try:
+                return datetime.strptime(value, "%Y-%m-%d")
+            except ValueError:
+                raise ValueError("Dates must be in format YYYY-MM-DD")
+        return value  
